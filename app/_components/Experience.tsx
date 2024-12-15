0fa8e395b1c7d41b635c4ec28ca1b5038fd2fcc0
Avinash <sanjayavinash55@gmail.com>
2024-12-15 11:05:02 +0530

    small change for responsiveness

--- app/_components/Experience.tsx
+++ app/_components/Experience.tsx
@@ -5,7 +5,7 @@ export default function Experience() {
 		<section className="mt-24 px-4 md:px-16">
 			<div className="flex flex-col md:flex-row justify-center gap-14">
 				{/* Left Column */}
-				<div className="main-menu-text flex flex-col max-w-full md:max-w-[40%]">
+				<div className="main-menu-text flex flex-col max-w-full md:max-w-[40%] mt-36">
 					<h2 className="text-3xl md:text-5xl font-bold text-[#006800] mb-4">
 						We have 14 Years of Experience
 					</h2>
@@ -32,110 +32,26 @@ export default function Experience() {
 }
 
 function ContactForm() {
-	const [formData, setFormData] = useState({
-		fullName: "",
-		phone: "",
-		email: "",
-		zipCode: "",
-	});
-
-	const handleChange = (e: { target: { name: any; value: any; }; }) => {
-		const { name, value } = e.target;
-		setFormData((prevData) => ({
-			...prevData,
-			[name]: value,
-		}));
-	};
-
-	const handleSubmit = (e: { preventDefault: () => void; }) => {
-		e.preventDefault();
-		// Handle form submission logic
-		console.log(formData);
-	};
-
 	return (
-		<section className="bg-[#3C3C3CFF] p-6 md:p-12 rounded-lg shadow-lg">
-			<h2 className="text-3xl text-center text-white mb-4">
-				Contact Us To Get A Quote!
-			</h2>
-			<p className="text-center text-white mb-6">
-				Enter your details below to claim your free measure & design
-				consultation (value $250)
-			</p>
-			<form onSubmit={handleSubmit} className="space-y-4">
-				<div className="flex flex-col">
-					<label htmlFor="fullName" className="text-white mb-2">
-						Full Name
-					</label>
-					<input
-						type="text"
-						id="fullName"
-						name="fullName"
-						value={formData.fullName}
-						onChange={handleChange}
-						className="p-3 rounded-lg border border-gray-300"
-						placeholder="Full Name"
-						required
-					/>
-				</div>
-
-				<div className="flex flex-col">
-					<label htmlFor="phone" className="text-white mb-2">
-						Personal Phone
-					</label>
-					<input
-						type="tel"
-						id="phone"
-						name="phone"
-						value={formData.phone}
-						onChange={handleChange}
-						className="p-3 rounded-lg border border-gray-300"
-						placeholder="Personal Phone"
-						required
-					/>
-				</div>
-
-				<div className="flex flex-col">
-					<label htmlFor="email" className="text-white mb-2">
-						Email
-					</label>
-					<input
-						type="email"
-						id="email"
-						name="email"
-						value={formData.email}
-						onChange={handleChange}
-						className="p-3 rounded-lg border border-gray-300"
-						placeholder="Email"
-						required
-					/>
-				</div>
-
-				<div className="flex flex-col">
-					<label htmlFor="zipCode" className="text-white mb-2">
-						Zip Code
-					</label>
-					<input
-						type="text"
-						id="zipCode"
-						name="zipCode"
-						value={formData.zipCode}
-						onChange={handleChange}
-						className="p-3 rounded-lg border border-gray-300"
-						placeholder="Zip Code"
-						required
-					/>
-				</div>
-
-				<div className="text-center">
-					<button
-						type="submit"
-						className="w-full py-3 bg-yellow-500 text-white font-bold rounded-lg hover:bg-yellow-400 transition duration-300"
-					>
-						Request Free Estimate
-					</button>
-				</div>
-			</form>
-		</section>
+		<div className="sm:w-1/2">
+			<iframe
+				src="https://api.leadconnectorhq.com/widget/form/qdSOxp9NXk8Ozs8j8z9e"
+				className="w-full h-100"
+				id="inline-qdSOxp9NXk8Ozs8j8z9e"
+				data-layout="{'id':'INLINE'}"
+				data-trigger-type="alwaysShow"
+				data-trigger-value=""
+				data-activation-type="alwaysActivated"
+				data-activation-value=""
+				data-deactivation-type="neverDeactivate"
+				data-deactivation-value=""
+				data-form-name="LP Form"
+				data-height="548"
+				data-layout-iframe-id="inline-qdSOxp9NXk8Ozs8j8z9e"
+				data-form-id="qdSOxp9NXk8Ozs8j8z9e"
+				title="LP Form"
+			></iframe>
+			<script src="https://link.msgsndr.com/js/form_embed.js"></script>
+		</div>
 	);
 }
--- app/_components/Findus.tsx
+++ app/_components/Findus.tsx
@@ -1,65 +1,84 @@
 export default function FindUs() {
 	return (
-		<div className="mt-16">
-			<h1 className="text-center text-4xl font-semibold text-[#006800] mx-12">Find Us On</h1>
-			<div className="findusimg flex justify-center gap-10">
+		<div className="mt-8">
+			<h1 className="text-center text-4xl font-semibold text-[#006800] mx-12 pb-2">
+				Find Us On
+			</h1>
+			<div className="findusimg flex flex-wrap justify-evenly gap-4 md:gap-10 mt-8">
 				<a
 					className="findus-anchor"
 					href="https://www.houzz.com/professionals/roofing-and-gutters/r-and-m-roofers-llc-pfvwus-pf~1547256125"
 					target="_blank"
+					rel="noopener noreferrer"
 				>
 					<img
 						src="./image/houzz.png"
-						alt=""
-						className="w-80 p-20"
+						alt="Houzz"
+						className=" w-36 sm:w-40 md:w-48 lg:w-64 p-4"
 					/>
 				</a>
 				<a
 					className="findus-anchor"
 					href="https://www.bbb.org/us/ny/hempstead/profile/roofing-contractors/r-m-roofers-llc-0121-87152151"
 					target="_blank"
+					rel="noopener noreferrer"
 				>
 					<img
 						src="./image/bbb.png"
-						alt=""
-						className="w-80 p-20"
+						alt="BBB"
+						className=" w-36 sm:w-40 md:w-48 lg:w-64 p-4"
 					/>
 				</a>
 				<a
 					className="findus-anchor"
 					href="https://www.showmelocal.com/profile.aspx?bid=24919773"
 					target="_blank"
+					rel="noopener noreferrer"
 				>
 					<img
 						src="./image/showMeLocal.png"
-						alt=""
-						className="w-80 p-20"
+						alt="ShowMeLocal"
+						className=" w-36 sm:w-40 md:w-48 lg:w-64 p-4"
 					/>
 				</a>
 				<a
 					className="findus-anchor"
 					href="https://www.mapquest.com/us/new-york/rm-roofers-420072327"
 					target="_blank"
+					rel="noopener noreferrer"
 				>
 					<img
 						src="./image/mapquest.png"
-						alt=""
-						className="w-80 p-20"
+						alt="MapQuest"
+						className=" w-36 sm:w-40 md:w-48 lg:w-64 p-4"
 					/>
 				</a>
 			</div>
 
-		<section className="bg-fixed" style={{ backgroundImage: "url('./image/roofing-bg.jpg"}}>
-        	<div className="flex flex-col mx-36">
-            	<div className="">
-                	<img src="/image/logo.png" alt="RM Roofing Logo" className="block; ml-auto mr-auto w-1/4" />
-	            </div>
-				<div className="cont-p">
-					<p className="text-center text-xl text-white">Why should you choose R&M Roofers LLC for your roofing needs? Because when you choose us, you're choosing 14 years of unwavering commitment to excellence. Our team is not just experienced; we are passionate about providing roofing solutions that protect your investment. We understand the significance of your roof, and we approach every project with meticulous care and expertise. </p>
+			<section
+				className="bg-fixed bg-no-repeat bg-cover w-full pb-12 mt-12"
+				style={{ backgroundImage: "url('./image/roofing-bg.jpg" }}
+			>
+				<div className="flex flex-col mx-36">
+					<div className="">
+						<img
+							src="/image/logo.png"
+							alt="RM Roofing Logo"
+							className="block; ml-auto mr-auto w-1/4"
+						/>
+					</div>
+					<div className="cont-p">
+						<p className="text-center text-xl text-white">
+							Why should you choose R&M Roofers LLC for your roofing needs?
+							Because when you choose us, you're choosing 14 years of unwavering
+							commitment to excellence. Our team is not just experienced; we are
+							passionate about providing roofing solutions that protect your
+							investment. We understand the significance of your roof, and we
+							approach every project with meticulous care and expertise.{" "}
+						</p>
+					</div>
 				</div>
-    	    </div>
-    	</section>
-
+			</section>
 		</div>
 	);
 }
