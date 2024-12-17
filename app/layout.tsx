87d533150b4468a389438f09090ae186db8f961b
Avinash <sanjayavinash55@gmail.com>
2024-12-18 00:15:26 +0530

    Errors Correct - FontAwesome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "@fortawesome/fontawesome-svg-core/styles.css";
 import Nav from "./_components/Nav";
 
 const geistSans = Geist({
